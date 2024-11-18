1d5dc4f6afdf55a32d9f1e814b7bbeb7a4746294
John Name <oluwatobilobasalawu1234@gmail.com>
2024-11-18 19:31:37 +0000

    using props in react

--- src/App.js
+++ src/App.js
@@ -5,18 +5,26 @@ const Person = (props) => {
     <>
       <h1>Name: {props.firstName}</h1>
       <h2>Last Name: {props.lastName}</h2>
-      <h2>Age: {props.Age}</h2>
+      <h2>Age: {props.age}</h2>
     </>
   )
 }
 
 const App = () => {
-  const name = 'Tobi';
 
   return (
     <div className="App">
-      <Person firstName={"Tobi"} lastName={"Salawu"} Age = {'30'}/>
-      <Person
+      <Person 
+        firstName="Tobi" 
+        lastName="Salawu" 
+        age={30}
+      />
+      <Person 
+        firstName="shf" 
+        lastName="Idk"
+        age={30}
+      />
+      <Person firstName="shs" lastName="Jaydah" age={30}/>
     </div>
   );
 }
