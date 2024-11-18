a72ed57e4408ca719a7a2a5a2cf2fef7993f9033
John Name <oluwatobilobasalawu1234@gmail.com>
2024-11-18 19:57:41 +0000

    creating a new project

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = () => {
+  return(
+    <div>
+      <h1>My React App!</h1>
+      <p>Welcome to my React app, loaded with Parcel.</p>
+    </div>
+  );
+}
+
+export default App
