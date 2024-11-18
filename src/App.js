ad651d411fff1c5c567073fa7b1c4040a013a48e
John Name <oluwatobilobasalawu1234@gmail.com>
2024-11-18 21:00:17 +0000

    Refactor CSS and add movie search functionality

--- src/App.js
+++ src/App.js
@@ -7,6 +7,14 @@ import SearchIcon from './search.svg'
 
 const apiUrl = "https://www.omdbapi.com/?apikey=c99e8c72";
 
+const movie1 = {
+  "Title": "My Hero Academia Abridged",
+  "Year": "2016–",
+  "imdbID": "tt7836396",
+  "Type": "series",
+  "Poster": "https://m.media-amazon.com/images/M/MV5BZGQxMzNhNmQtNzcyZS00M2UyLWI1ZDktMzI4ZjM4OTM4MWFlXkEyXkFqcGdeQXVyMTYwNTk5MDk@._V1_SX300.jpg"
+}
+
 const App = () => {
 
   const searchMovies = async (title) => {
@@ -26,7 +34,32 @@ const App = () => {
       <h1>StreamBase</h1>
 
       <div className='search'>
-        <input placeholder='Search Movies' />
+        <input placeholder='Search Movies' 
+        value= "SUPERMAN"
+        onChange={() => {}}/>
+      <img 
+        src={SearchIcon}
+        alt='Search Icon'
+        onClick={() => {}}
+      />
+      </div>
+
+      <div className='container'>
+        <div className='movie'>
+          <div>
+            <p>{movie1.Year}</p>
+          </div>
+
+        <div>
+          <img src={movie1.Poster !== 'N/A' ? movie1.Poster : 'https://via.placeholder.com/400'} alt={movie1.Title}/>
+        </div>
+
+        <div>
+          <span>{movie1.Type}</span>
+          <h3>{movie1.Title}</h3>
+        </div>
+
+        </div>
       </div>
     </div>
   );
