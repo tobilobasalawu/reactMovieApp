610b5008d49c4a9ef495b9d6410049ddd33b08b7
John Name <oluwatobilobasalawu1234@gmail.com>
2024-11-18 18:54:54 +0000

    Imported Modules

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+const App = () => {
+  const name = 'John';
+  const isNameShowing = false;
+
+  return (
+    <div className="App">
+      <h1>Konichiwa, {2 + 2 === 4 ?}</h1>
+    </div>
+  );
+}
+
+export default App;
