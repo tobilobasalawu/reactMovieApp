2ba1a16a064c6a783a5940f942480874b547ca6b
John Name <oluwatobilobasalawu1234@gmail.com>
2024-11-18 20:01:51 +0000

    setting up an api key for movie data

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react'
 
+//Api key: c99e8c72
+
+const apiUrl = "htttps://www.omdpapi.com/?apikey=c99e8c72";
+
 const App = () => {
   return(
     <div>
-      <h1>My React App!</h1>
-      <p>Welcome to my React app, loaded with Parcel.</p>
+
     </div>
   );
 }
