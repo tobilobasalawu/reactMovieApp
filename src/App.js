3c0534f9c8f9068952289d9489cb4eb5d2dc3815
John Name <oluwatobilobasalawu1234@gmail.com>
2024-11-18 20:08:41 +0000

    Refactor API URL and add movie search functionality

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
 import React from 'react'
+import { useEffect } from 'react';
 
 //Api key: c99e8c72
 
-const apiUrl = "htttps://www.omdpapi.com/?apikey=c99e8c72";
+const apiUrl = "https://www.omdbapi.com/?apikey=c99e8c72";
 
 const App = () => {
+
+  const searchMovies = async (title) => {
+    const response = await fetch((`${apiUrl}&s=${title}`)); //calling Api
+    const data = await response.json();
+
+    console.log(data);
+  };
+    
+
+  useEffect(() => {
+    searchMovies("MY HERO ACADEMIA");
+  }, []);
+
   return(
     <div>
-
+      <h1>Movie App</h1>
     </div>
   );
 }
